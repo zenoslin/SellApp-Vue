1ca53323adc1a3bb30f8bac6c95f5af21d020efb
zenos <zenoslin@outlook.com>
2018-04-04 01:05:46 +0800

    updata

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,7 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
     //强制分号
     'semi': ['error','always'],
-    'indent': 0
+    'indent': 0,
+    'space-before-function-paren': 0
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
 import App from './App';
 import goods from 'components/goods/goods';
 import ratings from 'components/ratings/ratings';
 import seller from 'components/seller/seller';
+
 import 'common/stylus/index.styl';
 
 Vue.use(VueRouter);
+Vue.use(VueResource);
 
 let app = Vue.extend(App);
 
