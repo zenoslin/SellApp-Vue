542921b465aa174a198160178925ee0c2ef739bd
zenos <zenoslin@outlook.com>
2018-04-08 15:04:23 +0800

    updata

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -2,30 +2,30 @@
 	<div class="goods">
 		<div class="menu-wrapper" v-el:menu-wrapper>
 			<ul>
-				<li v-for="item in goods" class="menu-item" :class="{'current':currentIndex === $index}" @click="selectMenu($index,$event)">
+				<li v-for="item in goods" class="menu-item" :class="{'current':currentIndex===$index}" @click="selectMenu($index,$event)">
 					<span class="text border-1px">
-						<span v-show="item.type > 0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
+						<span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
 					</span>
 				</li>
 			</ul>
 		</div>
 		<div class="foods-wrapper" v-el:foods-wrapper>
 			<ul>
-				<li v-for="item in goods" class="foot-list food-list-hook">
+				<li v-for="item in goods" class="food-list food-list-hook">
 					<h1 class="title">{{item.name}}</h1>
 					<ul>
-						<li @click="selectFood(food, $event)" v-for="food in item.foods" class="food-item border-1px">
+						<li @click="selectFood(food,$event)" v-for="food in item.foods" class="food-item border-1px">
 							<div class="icon">
-								<img width:"57" height="57" :src="food.icon">
+								<img width="57" height="57" :src="food.icon">
 							</div>
 							<div class="content">
 								<h2 class="name">{{food.name}}</h2>
 								<p class="desc">{{food.description}}</p>
 								<div class="extra">
-									<span class="count">月售{{food.sellCount}}份</span><span>月好评率{{food.rating}}%</span>
+									<span class="count">月售{{food.sellCount}}份</span><span>好评率{{food.rating}}%</span>
 								</div>
 								<div class="price">
-									<span class="now">¥{{food.price}}</span><span class="old" v-show="food.oldPrice">¥{{food.oldPrice}}</span>
+									<span class="now">￥{{food.price}}</span><span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
 								</div>
 								<div class="cartcontrol-wrapper">
 									<cartcontrol :food="food"></cartcontrol>
@@ -37,8 +37,8 @@
 			</ul>
 		</div>
 		<shopcart v-ref:shopcart :select-foods="selectFoods" :delivery-price="seller.deliveryPrice" :min-price="seller.minPrice"></shopcart>
-	</div>
-	<food :food="selectedFood" v-ref:food></food>
+		</div>
+		<food :food="selectedFood" v-ref:food></food>
 </template>
 
 <script type="text/ecmascript-6">
