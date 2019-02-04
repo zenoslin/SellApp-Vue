ebc68ff8d11786812491b889e1bc0e572227b2a4
zenos <zenoslin@outlook.com>
2019-02-04 13:53:38 +0800

    把1.0版本放到v1.0文件夹中

--- v1.0/prod.server.js
+++ v1.0/prod.server.js
@@ -21,35 +21,35 @@ var ratings = appData.ratings;
 
 var apiRoutes = express.Router();
 
-apiRoutes.get('/seller',function (req, res) {
+apiRoutes.get('/seller', function (req, res) {
   res.json({
     errno: 0,
     data: seller
   });
 });
 
-apiRoutes.get('/goods',function (req, res) {
+apiRoutes.get('/goods', function (req, res) {
   res.json({
     errno: 0,
     data: goods
   });
 });
 
-apiRoutes.get('/ratings',function (req, res) {
+apiRoutes.get('/ratings', function (req, res) {
   res.json({
     errno: 0,
     data: ratings
   });
 });
 
-app.use('/api', apiRoutes);
+app.use('sell/api', apiRoutes);
 
 app.use(express.static('./dist'));
 
 module.exports = app.listen(port, function (err) {
   if (err) {
-    console.log(err)
-    return
+    console.log(err);
+    return;
   }
-  console.log('Listening at http://localhost:' + port + '\n')
+  console.log('Listening at http://localhost:' + port + '\n');
 });
